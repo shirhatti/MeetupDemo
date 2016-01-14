544646b441fd1e7238b01ff1347645388b8b10a1
Sourabh Shirhatti <shirhatti@outlook.com>
2016-01-14 01:50:25 -0800

    Add logger

--- src/MeetupDemo/Controllers/HomeController.cs
+++ src/MeetupDemo/Controllers/HomeController.cs
@@ -3,14 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace MeetupDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private ILogger _logger;
+
+        public HomeController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<HomeController>();
+        }
         [Route("/")]
         public IActionResult Index()
         {
+            _logger.LogInformation("Log from inside controller");
             return View();
         }
     }
--- src/MeetupDemo/Startup.cs
+++ src/MeetupDemo/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace MeetupDemo
 {
@@ -18,8 +19,11 @@ namespace MeetupDemo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
+            loggerFactory.MinimumLevel = LogLevel.Information;
+            loggerFactory.AddConsole();
+
             app.UseMvc();
         }
 
